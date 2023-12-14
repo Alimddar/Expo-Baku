f5469e00e185f04fddae9b91961c0320a95cc2db
Rafi Alizade <refielizade3@gmail.com>
2023-12-14 21:34:04 +0400

    Register panel changed

--- frontend/assets/script/index.js
+++ frontend/assets/script/index.js
@@ -2,6 +2,14 @@ const yonlendir = (url) => {
     document.location.href = url;
 };
 
-document.getElementById('checkbox-login').onclick = function() {
-  document.getElementById('checkbox-login').parentElement.style.color = this.checked ? "black" : "red";
-};
+document.getElementById('custom-button').addEventListener('click', function() {
+  document.getElementById('hidden-file-input').click();
+});
+
+document.getElementById('hidden-file-input').addEventListener('change', function() {
+  // Handle the file selection
+  if (this.files.length > 0) {
+    console.log("Selected file:", this.files[0].name);
+    // You can add more logic here to handle the file
+  }
+});
