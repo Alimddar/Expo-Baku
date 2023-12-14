5f7e8882c6eedf372be027ed10cf5361b219d3c8
Rafi Alizade <refielizade3@gmail.com>
2023-12-14 21:45:52 +0400

    Register problem solving

--- frontend/assets/script/index.js
+++ frontend/assets/script/index.js
@@ -2,14 +2,21 @@ const yonlendir = (url) => {
     document.location.href = url;
 };
 
-document.getElementById('custom-button').addEventListener('click', function() {
+document.getElementById('custom-button').addEventListener('click', function(event) {
+  event.preventDefault(); // Varsayılan form gönderme işlemini engelle
   document.getElementById('hidden-file-input').click();
 });
 
 document.getElementById('hidden-file-input').addEventListener('change', function() {
-  // Handle the file selection
-  if (this.files.length > 0) {
-    console.log("Selected file:", this.files[0].name);
-    // You can add more logic here to handle the file
+  if (this.files && this.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var imageDisplay = document.getElementById('image-display');
+      imageDisplay.innerHTML = ''; // Önceki resmi temizle
+      var img = new Image();
+      img.src = e.target.result;
+      imageDisplay.appendChild(img);
+    };
+    reader.readAsDataURL(this.files[0]);
   }
 });
