7552c093cbce87619a3ff43c7591d5d01b0d83aa
Rafi Alizade <refielizade3@gmail.com>
2023-12-15 00:49:47 +0400

    Register panel problems solved / Responsive solving

--- frontend/assets/script/index.js
+++ frontend/assets/script/index.js
@@ -1,22 +1,29 @@
-const yonlendir = (url) => {
+document.addEventListener('DOMContentLoaded', function() {
+
+  document.getElementById('userImage').addEventListener('click', function(event) {
+      event.preventDefault();
+      document.getElementById('hidden-file-input').click();
+  });
+
+  document.getElementById('hidden-file-input').addEventListener('change', function() {
+      if (this.files && this.files[0]) {
+          var reader = new FileReader();
+          reader.onload = function(e) {
+              var imageDisplay = document.getElementById('register-page_inner_left_form__upl_img_image-display');
+              imageDisplay.innerHTML = '';
+              var img = new Image();
+              img.src = e.target.result;
+              img.style.width = '128px';
+              img.style.height = '128px';
+              img.style.borderRadius = '50%';
+              img.style.objectFit = 'fill';
+              imageDisplay.appendChild(img);
+          };
+          reader.readAsDataURL(this.files[0]);
+      }
+  });
+});
+
+function yonlendir(url) {
     document.location.href = url;
-};
-
-document.getElementById('custom-button').addEventListener('click', function(event) {
-  event.preventDefault(); // Varsayılan form gönderme işlemini engelle
-  document.getElementById('hidden-file-input').click();
-});
-
-document.getElementById('hidden-file-input').addEventListener('change', function() {
-  if (this.files && this.files[0]) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      var imageDisplay = document.getElementById('image-display');
-      imageDisplay.innerHTML = ''; // Önceki resmi temizle
-      var img = new Image();
-      img.src = e.target.result;
-      imageDisplay.appendChild(img);
-    };
-    reader.readAsDataURL(this.files[0]);
-  }
-});
+}
